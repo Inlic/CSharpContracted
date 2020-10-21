8e7904616acf682d01f59db76b66951e11b4716d
Ben W <bwiedeback@gmail.com>
2020-10-21 09:54:02 -0600

    vue front end and authorization

--- Startup.cs
+++ Startup.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using CodeWorks.Auth0Provider;
 using CSharpContracted.Repositories;
 using CSharpContracted.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -31,6 +33,31 @@ namespace CSharpContracted
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+
+      services.AddAuthentication(options =>
+              {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+              }).AddJwtBearer(options =>
+                {
+                  options.Authority = $"https://{Configuration["Auth0:Domain"]}/";
+                  options.Audience = Configuration["Auth0:Audience"];
+                });
+      services.AddCors(options =>
+      {
+        options.AddPolicy("CorsDevPolicy", builder =>
+              {
+                builder
+                          .WithOrigins(new string[]{
+                            "http://localhost:8080",
+                            "http://localhost:8081"
+                          })
+                          .AllowAnyMethod()
+                          .AllowAnyHeader()
+                          .AllowCredentials();
+              });
+      });
+
       services.AddControllers();
       services.AddScoped<IDbConnection>(x => CreateDBContext());
       services.AddTransient<JobsRepository>();
@@ -56,11 +83,16 @@ namespace CSharpContracted
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
+        app.UseCors("CorsDevPolicy");
       }
+
+      Auth0ProviderExtension.ConfigureKeyMap(new List<string>() { "id" });
 
       app.UseHttpsRedirection();
 
       app.UseRouting();
+
+      app.UseAuthentication();
 
       app.UseAuthorization();
 
